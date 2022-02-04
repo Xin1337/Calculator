99bf390c17a10e8f36cc93432c0655712efb4e49
Nix <drlftg0d321@gmail.com>
2022-02-04 14:10:31 +0800

    Initial commit

---
+++ src/com/nix/Main.java
@@ -0,0 +1,51 @@
+package com.nix;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int x;
+    public static int y;
+
+    public static int addition() {
+        return (x + y);
+    }
+
+    public static int subtraction() {
+        return (x - y);
+    }
+
+    public static int multiplication() {
+        return (x * y);
+    }
+
+    public static int division() {
+        return (x / y);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number 1: ");
+        x = sc.nextInt();
+        System.out.print("Enter Number 2: ");
+        y = sc.nextInt();
+
+        System.out.println("[1] for Addition");
+        System.out.println("[2] for Subtraction");
+        System.out.println("[3] for Multiplication");
+        System.out.println("[4] for Division");
+
+        String resp = sc.next();
+
+        if (resp.equals("1")) {
+            System.out.print(addition());
+        } else if (resp.equals("2")) {
+            System.out.print(subtraction());
+        } else if (resp.equals("3")) {
+            System.out.print(multiplication());
+        } else if (resp.equals("4")) {
+            System.out.print(division());
+        }
+    }
+}
